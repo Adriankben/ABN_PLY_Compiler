ee0948d549848de1d6ea79e292fbf324740500b4
Adrian Bennett <34956924+Adriankben@users.noreply.github.com>
2023-03-24 20:55:37 -0500

    Add files via upload

--- compiler.py
+++ compiler.py
@@ -1,8 +1,8 @@
-Copyright (c) <year>, <copyright holder>
-All rights reserved.
+# Copyright (c) 2023, Adrian Bennett
+# All rights reserved.
 
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree. 
+# This source code is licensed under the Apache-style license found in the
+# LICENSE file in the root directory of this source tree. 
 
 # Adrian Bennett 1800826 
 # Brianna Philp 1903475 
